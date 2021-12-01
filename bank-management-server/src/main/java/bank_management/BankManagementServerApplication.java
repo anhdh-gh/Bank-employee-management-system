8e29584a29294f7a11031e31ed42f8f95fb9638e
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-12-02 00:34:59 +0700

    init

---
+++ bank-management-server/src/main/java/bank_management/BankManagementServerApplication.java
@@ -0,0 +1,13 @@
+package bank_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementServerApplication.class, args);
+	}
+
+}
---
+++ bank-management-server/src/test/java/bank_management/BankManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bank_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
