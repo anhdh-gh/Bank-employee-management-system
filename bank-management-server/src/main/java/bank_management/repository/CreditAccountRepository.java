257fa19f426f75d62186b23c1134ecc77fee6bc0
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-12-10 22:36:11 +0700

    Push

--- bank-management-server/src/main/java/bank_management/repository/CreditAccountRepository.java
+++ bank-management-server/src/main/java/bank_management/repository/CreditAccountRepository.java
@@ -2,9 +2,11 @@ package bank_management.repository;
 
 import bank_management.entity.BankAccount;
 import bank_management.entity.CreditAccount;
+import bank_management.entity.PaymentAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CreditAccountRepository extends JpaRepository<CreditAccount, String>{
+    CreditAccount getCreditAccountByCustomerID(String customerID);
 }
