ef66bf1148ad40201e05277c2484f0118bdd695c
Sasuke0508 <76610320+Sasuke0508@users.noreply.github.com>
2021-12-11 01:35:58 +0700

    Create ConvertDate

---
+++ bank-management-client/assets/js/common/convertDate.js
@@ -0,0 +1,15 @@
+function convertDate(dateInput, option){
+    // Nếu convert Datetime (lấy từ server) sang date
+    if(option == 1){
+        dateInput = new Date(dateInput);
+        dateInput = dateInput.toLocaleDateString();
+    }
+    return dateInput.split("-").reverse().join("-");
+}
+
+// let dateTest = "2000-08-05";
+// console.log(convertDate(dateTest, 2));
+
+// let dateTimeTest = "2011-07-14 11:23:00";
+// console.log(convertDate(dateTimeTest, 1));
+
