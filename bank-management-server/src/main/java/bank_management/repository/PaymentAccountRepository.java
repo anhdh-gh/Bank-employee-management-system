33b8fe4a84326f9072e4b193ed03788149750d75
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-12-12 14:12:05 +0700

    Push

--- bank-management-server/src/main/java/bank_management/repository/PaymentAccountRepository.java
+++ bank-management-server/src/main/java/bank_management/repository/PaymentAccountRepository.java
@@ -4,9 +4,15 @@ import bank_management.entity.BankAccount;
 import bank_management.entity.Customer;
 import bank_management.entity.PaymentAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentAccountRepository extends JpaRepository<PaymentAccount, String> {
     PaymentAccount getPaymentAccountByCustomerID(String customerID);
+
+    @Modifying
+    @Query(value = "DELETE FROM paymentaccount WHERE BankAccountID = ?1", nativeQuery = true)
+    int deletePaymentAccountByID(String id);
 }
