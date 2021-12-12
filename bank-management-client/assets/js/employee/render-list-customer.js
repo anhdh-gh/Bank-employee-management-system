a77af6c708c3b80fe322906f918dcda580dcb4f1
Sasuke0508 <76610320+Sasuke0508@users.noreply.github.com>
2021-12-12 16:07:46 +0700

    Done Search

--- bank-management-client/assets/js/employee/render-list-customer.js
+++ bank-management-client/assets/js/employee/render-list-customer.js
@@ -46,16 +46,11 @@ const renderData = (data) => {
                         >
                           <i class="far fa-edit"></i>
                         </a>
-                        <button
-                          type="submit"
-                          data-toggle="modal"
-                          data-target="#delete_customer"
-                          class="btn btn-danger btn-sm mb-1" class="delete" value="${
-                            customer.id
-                          }"
-                        >
-                          <i class="far fa-trash-alt"></i>
-                        </button>
+                        <a href="customer-profile.html?id=${
+                          customer.id
+                        }" class="btn btn-info btn-sm mb-1">
+                        <i class="far fa-eye"></i>
+                        </a>
                       </td>
                     </tr>`;
     html += customerRow;
@@ -74,32 +69,62 @@ const paging = () => {
   })(jQuery);
 };
 
-ApiClient.get("/customer", {})
-  .then((resp) => {
-    let data = resp.data.data;
-    renderData(data);
-    paging();
-  })
-  .catch((err) => {
-    Notify.showError(err.response.data.data.message);
-  });
+// ApiClient.get("/customer", {})
+//   .then((resp) => {
+//     let data = resp.data.data;
+//     renderData(data);
+//     paging();
+//   })
+//   .catch((err) => {
+//     Notify.showError(err.response.data.data.message);
+//   });
 
 // Search Customer
 
-$(idBtnSearch).click((e) => {
-  e.preventDefault();
-  const dataSearch = Form.getData(idFormSearchCustomer);
-  // console.log(dataSearch);
-  ApiClient.get("/customer/search", dataSearch)
-    .then((resp) => {
-      let dataResult = resp.data.data;
-      if (dataResult.length === 0) Notify.showError("Không tìm thấy customer!");
-      else {
-        renderData(dataResult);
-        // paging();
-      }
-    })
-    .catch((err) => {
-      Notify.showError(err.message);
-    });
-});
+// Hiển thị List bank account
+let urlApi = "/customer";
+
+// Get các tham số trên đường dẫn
+const paramsSearch = getUrlVars();
+if (paramsSearch) {
+  if (paramsSearch.type) {
+    $("#select2-type-container").text(paramsSearch.type);
+    if (paramsSearch.type === "All") paramsSearch.type = "";
+  }
+
+  urlApi += "/search?" + encodeQueryData(paramsSearch);
+  Object.keys(paramsSearch).forEach((att) => {
+    $(`#${att}`).val(paramsSearch[att]);
+  });
+}
+
+ApiClient.get(urlApi)
+  .then((resp) => {
+    const data = resp.data.data;
+    renderData(data);
+    paging();
+    if (data.length == 0) {
+      Notify.showError("Không tìm thấy dữ liệu!");
+    }
+  })
+  .catch((err) => {
+    Notify.showError(err.response.data.message);
+  });
+
+// $(idBtnSearch).click((e) => {
+//   e.preventDefault();
+//   const dataSearch = Form.getData(idFormSearchCustomer);
+//   // console.log(dataSearch);
+//   ApiClient.get("/customer/search", dataSearch)
+//     .then((resp) => {
+//       let dataResult = resp.data.data;
+//       if (dataResult.length === 0) Notify.showError("Không tìm thấy customer!");
+//       else {
+//         renderData(dataResult);
+//         // paging();
+//       }
+//     })
+//     .catch((err) => {
+//       Notify.showError(err.message);
+//     });
+// });
