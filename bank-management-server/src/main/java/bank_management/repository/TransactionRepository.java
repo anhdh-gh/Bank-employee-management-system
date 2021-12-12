f4b555e89be1c3fdecfe8d170538cd9ed25b4ca8
Nguyễn Văn Toàn <54373203+ToanCoi@users.noreply.github.com>
2021-12-12 15:46:59 +0700

    Update TransactionRepository.java

--- bank-management-server/src/main/java/bank_management/repository/TransactionRepository.java
+++ bank-management-server/src/main/java/bank_management/repository/TransactionRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String>{
     @Query( nativeQuery = true, value = "SELECT t.* FROM Transaction t WHERE bankAccountReceiveID = :BankAccountReceiveID ORDER BY t.createDate LIMIT 1")
     Transaction getAmountOfFirstTransactionByPaymentAccount(@Param(value = "BankAccountReceiveID") String paymentAccountID);
+
+    @Query("SELECT t FROM Transaction t WHERE t.bankAccountReceive.ID = ?1 OR t.bankAccountSent.ID = ?1")
+    List<Transaction> getTransactionsByBankAccountID(String bankAccountID);
 }
